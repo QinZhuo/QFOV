08888b246c75ec8f818bd5a1c4b0f523fa29d276
琴卓 <1043665802@qq.com>
2022-12-25 00:49:52 +0800

    QDemo

--- Demo/QFovTest.cs
+++ Demo/QFovTest.cs
@@ -11,8 +11,8 @@ namespace QTool.FOV
         public Transform moveTarget;
         private void Update()
         {
-			moveTarget.position += QDemo.MoveDirection * 2 * Time.deltaTime;
-			transform.LookAt(Camera.main.ScreenPointToRay(QDemo.MousePosition).RayCastPlane(Vector3.up, Vector3.zero));
+			moveTarget.position +=new Vector3( QDemo.MoveDirection.x,0,QDemo.MoveDirection.y) * 2 * Time.deltaTime;
+			transform.LookAt(Camera.main.ScreenPointToRay(QDemo.PointerPosition).RayCastPlane(Vector3.up, Vector3.zero));
         }
     }
     
